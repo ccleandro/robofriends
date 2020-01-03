0013990f084a50014b5bf76fc27391a0fd9c4397
Leandro Rodrigues <cc.leandro@gmail.com>
2020-01-03 11:25:13 -0300

    Updates

---
+++ precache-manifest.5eb6d2554e2596c616362c01b73bffc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1623e1925f76ec6b27b697e632ea3f",
+    "url": "/ccleandro/robofriends.git/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/ccleandro/robofriends.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0cd7cc5bc398f02880c0",
+    "url": "/ccleandro/robofriends.git/static/css/main.aa0d5537.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/ccleandro/robofriends.git/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ccleandro/robofriends.git/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cd7cc5bc398f02880c0",
+    "url": "/ccleandro/robofriends.git/static/js/main.95095239.chunk.js"
+  },
+  {
+    "revision": "2e586854fb02b944f42e",
+    "url": "/ccleandro/robofriends.git/static/js/runtime-main.9028bc7e.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/ccleandro/robofriends.git/static/media/SEGA.82e09f78.TTF"
+  }
+]);
