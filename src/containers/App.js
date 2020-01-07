644e5acfde4d0be7319e2840ead9e61df4faa477
Leandro Rodrigues <cc.leandro@gmail.com>
2020-01-07 14:59:37 -0300

    redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,61 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 //import { robots } from "./robots";
+import { setSearchField, requestRobots } from "../actions";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  };
+};
 
 class App extends Component {
-  constructor() {
+  /*   constructor() {
     super();
     this.state = {
       //robots: robots,
-      robots: [],
-      searchfield: ""
+      robots: []
+      //searchfield: ""
     };
-  }
+  } */
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
+    //console.log(this.props.store.getState());
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then(response => response.json())
+    //   .then(users => this.setState({ robots: users }));
   }
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
-  };
+  // onSearchChange = event => {
+  //   this.setState({ searchfield: event.target.value });
+  // };
   render() {
-    const { robots, searchfield } = this.state;
+    //const { robots, searchfield } = this.state;
+    //const { robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return !robots.length ? (
+    return isPending ? (
       <h1>Loading...</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -43,5 +65,5 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+//connect() is a high order function, it returns a function. functionReturned(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
